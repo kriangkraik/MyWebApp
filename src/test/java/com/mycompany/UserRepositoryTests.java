1ce46e88e75a97553a61063a7d98dde3c2ec5b0d
ความสุขเกิด จากสิ่งเล็กๆ <BANK34076@hotmail.com>
2022-03-19 13:51:45 +0700

    first commit

---
+++ src/test/java/com/mycompany/UserRepositoryTests.java
@@ -0,0 +1,37 @@
+package com.mycompany;
+
+import com.company.user.User;
+import com.company.user.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+    @Autowired private UserRepository repo;
+
+    @Test
+    public void testAddNew(){
+        User user = new User();
+        user.setEmail("kriangkraiketkun@gmail.com");
+        user.setPassword("12345678");
+        user.setFirstName("Kriangkrai");
+        user.setLastname("Ketkun");
+        User savedUser = repo.save(user);
+        Assertions.assertThat(savedUser).isNotNull();
+        Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+    @Test
+    public void testListAll(){
+        Iterable<User> users = repo.findAll();
+        Assertions.assertThat(users).hasSizeGreaterThan(0);
+        for (User user : users) {
+            System.out.println(user);
+        }
+    }
+}
